34cb0edffd19e3a24d2f20ff9ab62647b2df3266
Aelpxy <aelpxy@protonmail.com>
2022-04-08 18:16:45 +0600

    feat: get value by ID

--- main.go
+++ main.go
@@ -16,18 +16,27 @@ var testData = []Data{
 }
 
 func main() {
-
 	  router := gin.Default()
-
 
 		router.GET("/", getData)
 		router.POST("/", postData)
+		router.GET("/:id", getDataById)
 
 		router.Run("localhost:8080")
 }
 
 func getData(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, testData)
+}
+
+func getDataById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range testData {
+			if a.Key == id {
+				c.IndentedJSON(http.StatusOK, a)
+			}
+		}
 }
 
 func postData(c *gin.Context) {
