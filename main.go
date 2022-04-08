5a1ac26444a6cfadc3f27ac48770e57e60e1ec19
Aelpxy <aelpxy@protonmail.com>
2022-04-08 18:02:23 +0600

    Feat: Gin is now used

--- main.go
+++ main.go
@@ -1,51 +1,43 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"math/rand"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-var Data = []Value{}
-
-type Value struct {
-	Key   int    `json:"key"`
+type Data struct {
+	Key string `json:"key"`
 	Value string `json:"value"`
 }
 
+var testData = []Data{
+	{Key: "1", Value: "TestValue" },
+}
+
 func main() {
-	log.Println("vekDB Starting...")
 
-	http.HandleFunc("/create", postValue)
-	http.HandleFunc("/get", getValue)
+	  router := gin.Default()
 
-	log.Println("vekDB Started!")
-	http.ListenAndServe(":3000", nil)
+
+		router.GET("/", getData)
+		router.POST("/", postData)
+
+		router.Run("localhost:8080")
 }
 
-func getValue(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	json.NewEncoder(w).Encode(Data)
+func getData(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, testData)
 }
 
-func postValue(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	body, err := ioutil.ReadAll(r.Body)
+func postData(c *gin.Context) {
+	var newData Data
 
-	if err != nil {
-		log.Panic(err)
+	if err := c.BindJSON(&newData); err != nil {
+		return
 	}
 
-	var Value Value
-	json.Unmarshal(body, &Value)
+	testData = append(testData, newData)
 
-	Value.Key = rand.Intn(100)
-
-	Data = append(Data, Value)
-	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode("key created!")
+	c.IndentedJSON(http.StatusCreated, newData)
 }
